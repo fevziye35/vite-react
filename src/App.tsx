d5b4372efd2a03071a77436572d35a6f0d752704
fevziye35 <fevziye35@gmail.com>
2026-03-16 12:00:10 +0300

    Orijinal sayfalar geri yüklendi

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Sidebar from './components/layout/Sidebar'; 
 import DealsPage from './pages/DealsPage';
-import LoginPage from './pages/LoginPage';
-import AdminPage from './pages/AdminPage';
 import MessagesPage from './pages/MessagesPage';
 import ProformalarPage from './pages/ProformalarPage';
+import LoginPage from './pages/LoginPage';
+import AdminPage from './pages/AdminPage';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="flex min-h-screen bg-gray-50">
-        {/* Yan Menü (Sidebar) Artık Burada Görünecek */}
         <Sidebar /> 
-        
-        {/* Sağ Taraftaki Ana İçerik */}
         <main className="flex-1 ml-64 p-8">
           <Routes>
             <Route path="/" element={<Navigate to="/deals" replace />} />
@@ -22,7 +19,6 @@ function App() {
             <Route path="/proformas" element={<ProformalarPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/admin" element={<AdminPage />} />
-            {/* Eğer başka sayfaların varsa buraya eklemeye devam edeceğiz */}
           </Routes>
         </main>
       </div>
