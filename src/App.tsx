3bc23dc0a5cee652012925672c335a2fce903f33
fevziye35 <fevziye35@gmail.com>
2026-03-16 11:29:58 +0300

    Sidebar ve ana yapi geri yuklendi

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,29 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<DealsPage />} />
-        <Route path="/deals" element={<DealsPage />} />
+        <Route pathimport { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar'; // Sidebar bileşenini geri çağırıyoruz
+import DealsPage from './pages/DealsPage';
+// Buraya hata vermeyen diğer sayfalarını da ekleyebilirsin
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="flex min-h-screen bg-[#0f172a]">
+        <Sidebar /> {/* Menü geri geldi! */}
+        <main className="flex-1 ml-64 p-8">
+          <Routes>
+            <Route path="/" element={<DealsPage />} />
+            <Route path="/deals" element={<DealsPage />} />
+            {/* Diğer sayfalar hata vermediği sürece buraya eklenecek */}
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;="/deals" element={<DealsPage />} />
         <Route path="*" element={<DealsPage />} />
       </Routes>
     </BrowserRouter>
