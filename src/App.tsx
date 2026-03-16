7bce78a7690d8729b6830f627fc91b16a772cfa0
fevziye35 <fevziye35@gmail.com>
2026-03-16 11:24:38 +0300

    api.ts dosyasindaki hatali App fonksiyonu silindi

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DealsPage from './pages/DealsPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Vercel build alsın diye diğer yolları DealsPage'e yönlendiriyoruz */}
         <Route path="/" element={<DealsPage />} />
         <Route path="/deals" element={<DealsPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<DealsPage />} />
       </Routes>
     </BrowserRouter>
   );
